f4eef4f4068f7e81712b7b0f1c80afc9abf2d263
tinnist <82404492+tinnist@users.noreply.github.com>
2021-04-12 13:36:05 -0400

    added requirements for task

--- main.ts
+++ main.ts
@@ -1,8 +1,17 @@
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    agent.y += -3
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    agent.setPosition(78, 59)
+})
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     agent.x += -3
 })
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     agent.x += 3
+})
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    agent.y += 3
 })
 let agent: Sprite = null
 agent = sprites.create(img`
